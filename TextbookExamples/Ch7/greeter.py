55d011fc5204be2eb759612e7527beb1377d2324
j0e <j0e@T480s.thinkpad>
2024-06-22 17:04:39 -0700

    started ch7

---
+++ TextbookExamples/Ch7/greeter.py
@@ -0,0 +1,10 @@
+prompt = "Tell us who you are!"
+prompt += "\nWhat is your name? "
+
+name = input(prompt)
+print(f"Hello, {name}!")
+
+prompt2 = "How old are you? "
+age = input(prompt2)
+print(f"You're {age}")
+
